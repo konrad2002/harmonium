1ad2135611f2ddc6532db1d27b1037c96741db76
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-05-22 15:10:00 +0200

    add manual components; remove default style and content

---
+++ src/components/Manual/Manual.tsx
@@ -0,0 +1,12 @@
+import ManualKey from "../ManualKey/ManualKey.tsx";
+
+export default function Manual() {
+  return (
+    <>
+      <ManualKey color="red" />
+      <ManualKey color="yellow" />
+      <ManualKey color="blue" />
+      <ManualKey color="white" />
+    </>
+  )
+}
---
+++ src/components/ManualKey/ManualKey.tsx
@@ -0,0 +1,9 @@
+
+export default function ManualKey({color: string}) {
+  return (
+    <>
+      <div className={"manual-key"}>
+      </div>
+    </>
+  )
+}
